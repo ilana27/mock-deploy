3c7a2cd7d3246090c93a9e42b5add9a4e4272c19
Sarah Ridley <sarah_ridley@brown.edu>
2023-10-06 19:39:59 -0400

    refactor: adding /node-modules to gitignore

--- mock/src/components/REPLHistory.tsx
+++ mock/src/components/REPLHistory.tsx
@@ -1,16 +1,25 @@
-import '../styles/main.css';
+import "../styles/main.css";
 
-interface REPLHistoryProps{
-    // TODO: Fill with some shared state tracking all the pushed commands
-    history: string[],
-    mode: boolean
+interface REPLHistoryProps {
+  // TODO: Fill with some shared state tracking all the pushed commands
+  history: string[];
+  mode: boolean;
 }
-export function REPLHistory(props : REPLHistoryProps) {
-    return (
-        <div className="repl-history">
-            {/* This is where command history will go */}
-            {/* TODO: To go through all the pushed commands... try the .map() function! */}
-            {props.history.map((command, index) => props.mode ? <p>{command}</p> : <div><p>Command: {command}</p><p>Ouptut: fake output</p></div>)}
-        </div>
-    );
+export function REPLHistory(props: REPLHistoryProps) {
+  return (
+    <div className="repl-history">
+      {/* This is where command history will go */}
+      {/* TODO: To go through all the pushed commands... try the .map() function! */}
+      {props.history.map((command, index) =>
+        props.mode ? (
+          <p>{command}</p>
+        ) : (
+          <div>
+            <p>Command: {command}</p>
+            <p>Ouptut: fake output</p>
+          </div>
+        )
+      )}
+    </div>
+  );
 }
